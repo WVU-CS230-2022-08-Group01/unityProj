56d5bbfce126cee1ef854bc3c1c9376d51b91913
cmp00006 <cmp00006@mix.wvu.edu>
2022-11-10 18:47:23 -0500

    updated file reading

--- Assets/Scenes/Scripts/FileReading.cs
+++ Assets/Scenes/Scripts/FileReading.cs
@@ -4,22 +4,26 @@ using UnityEngine;
 using System.IO;
 using System;
 
+
+
 public class FileReading : MonoBehaviour
 {
- 
-
-
     // Start is called before the first frame update
     void Start()
     {
-        //ReadFile("readTest.txt", Application.dataPath + "/" + "readTest.txt");
+        Debug.Log("start function");
+        ReadFile("readTest.txt");
     }
-
-
+//string[], string[], string[]
     public void ReadFile(string fileName)
     {
+        Debug.Log("file reading function");
         string filePath = Application.dataPath + "/" + fileName;
-        ReadFile("readTest.txt", filePath);
+        Debug.Log(fileName);
+        Debug.Log(filePath);
+       
+
+
 
         /* fileName = "readTest.txt";
          filePath = Application.dataPath + "/" + fileName;
@@ -27,64 +31,58 @@ public class FileReading : MonoBehaviour
 
         //Variable for name of file to be read
 
-
         //Initialize empty lists
-        var questionList = new List<string>();
-        var rightAnswerList = new List<string>();
-        var wrongAnswerList = new List<string>();
+        List<string> questionList = new List<string>();
+        List<string> rightAnswerList = new List<string>();
+        List<string> wrongAnswerList = new List<string>();
         //var wrongAnswerListList = new List<string>();
 
-
-        int n = 0;
-        int b = 0;
-
-    //Read all lines of text file
-        string[] fileLines = File.ReadAllLines(filePath);
-
-
-        foreach(string fileLine in fileLines)
+        //Read file, iterate through one line at a time
+        foreach (string fileLine in System.IO.File.ReadLines(@filePath))
         {
-            questionList.Add(fileLine.Split(',', StringSplitOptions.RemoveEmptyEntries));
-            rightAnswerList.Add(fileLine.Split(',', StringSplitOptions.RemoveEmptyEntries));
-            wrongAnswerList.Add(fileLine.Split(',', StringSplitOptions.RemoveEmptyEntries));
-            wrongAnswerList.Add(fileLine.Split(',', StringSplitOptions.RemoveEmptyEntries));
-            wrongAnswerList.Add(fileLine.Split(',', StringSplitOptions.RemoveEmptyEntries));
+            //Split line at the commas, create array with each element a comma separated string
+            string[] lineArr = fileLine.Split(',');
 
+            Debug.Log(lineArr[0]);
+            Debug.Log(lineArr[1]);
+            Debug.Log(lineArr[2]);
+            Debug.Log(lineArr[3]);
+            Debug.Log(lineArr[4]);
 
-            /* questionArr[n]=fileLine.Split(',', StringSplitOptions.RemoveEmptyEntries);
-             rightAnswerArr[n]=fileLine.Split(',', StringSplitOptions.RemoveEmptyEntries);
-            wrongAnswerArr[b]=fileLine.Split(',', StringSplitOptions.RemoveEmptyEntries);
-             wrongAnswerArr[b+1] = fileLine.Split(',', StringSplitOptions.RemoveEmptyEntries);
-             wrongAnswerArr[b+2] = fileLine.Split(',', StringSplitOptions.RemoveEmptyEntries);*/
-
-            string[] questionArr=questionList.ToArray();
-            string[] rightAnswerArr=rightAnswerList.ToArray();
-           
-
-            print(questionArr[n]);
-            print(rightAnswerArr[n]);
-
-                n++;
-            b = b + 3;
-
-        }
-      /* using (StreamReader reader = new StreamReader(filename))
-        {
-            while(!reader.EndOfStream) //while reading the file, we haven't reached the end
-            {
-                //keep reading
-                Debug.Log(reader.ReadLine());  //read line, print to console
-
-            }
-        }*/
-    }
-
-   /* private void WriteFile()
-    {
-        using (StreamWriter writer = new StreamWriter(filename))
-        {
+            //Add element from array to respective list
             
-            writer.WriteLine(i.ToString());
+            questionList.Add(lineArr[0]);
+            rightAnswerList.Add(lineArr[1]);
+            wrongAnswerList.Add(lineArr[2]);
+            wrongAnswerList.Add(lineArr[3]);
+            wrongAnswerList.Add(lineArr[4]);
         }
-    }*/
+        //Convert lists to respective arrays
+       // print(questionList);
+
+        string[] questionArr = questionList.ToArray();
+        string[] rightAnswerArr = rightAnswerList.ToArray();
+        string[] wrongAnswerArr = wrongAnswerList.ToArray();
+
+       // print(questionArr);
+
+        //Create 2d Array with 
+
+       // return (questionArr, rightAnswerArr, wrongAnswerArr);
+
+    }
 }
+
+
+/*questionList.Add(fileLine.Split(',', StringSplitOptions.RemoveEmptyEntries));
+rightAnswerList.Add(fileLine.Split(',', StringSplitOptions.RemoveEmptyEntries));
+wrongAnswerList.Add(fileLine.Split(',', StringSplitOptions.RemoveEmptyEntries));
+wrongAnswerList.Add(fileLine.Split(',', StringSplitOptions.RemoveEmptyEntries));
+wrongAnswerList.Add(fileLine.Split(',', StringSplitOptions.RemoveEmptyEntries));*/
+
+
+/* questionArr[n]=fileLine.Split(',', StringSplitOptions.RemoveEmptyEntries);
+ rightAnswerArr[n]=fileLine.Split(',', StringSplitOptions.RemoveEmptyEntries);
+wrongAnswerArr[b]=fileLine.Split(',', StringSplitOptions.RemoveEmptyEntries);
+ wrongAnswerArr[b+1] = fileLine.Split(',', StringSplitOptions.RemoveEmptyEntries);
+ wrongAnswerArr[b+2] = fileLine.Split(',', StringSplitOptions.RemoveEmptyEntries);*/
