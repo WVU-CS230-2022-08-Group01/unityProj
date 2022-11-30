7153b1d6efaeb381b8a639698ed43684d9d7df73
BrianTech09 <90514604+BrianTech09@users.noreply.github.com>
2022-11-29 21:02:04 -0500

    Changes for the results page.
    The code that was changed during class time.

--- Assets/Scenes/Scripts/ScoringManage.cs
+++ Assets/Scenes/Scripts/ScoringManage.cs
@@ -1,34 +1,36 @@
-// using System.Collections;
-// using System.Collections.Generic;
-// using UnityEngine;
-// using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
 
-// public class ScoringManage : MonoBehaviour
-// {
-//    public static ScoringManage instance;
-
-//    //Knowing the score
-//    public Text pointsText;
-   
-//    // Start off with 0 points 
-//    int score = 0;
-
-//     //Start of the quiz it will know to keep score 
-//     private void Awake(){
-//         instance = this;
-//     }
-//     //Starting to earn points 
-//     void Start(){
-//         scoreText.text = score.ToString() + "Points"
-//     }
+public class ScoringManage : MonoBehaviour
+{
     
-//     // Adding on your points 
-//     public void AddPoints(){
-//         score += 1;
-//         scoreText.text = score.ToString() + "Points"
-//    }
+   public static ScoringManage instance;
+    Score track = GameObject.Find("Score").GetComponent<Score>();
+   //Knowing the score
+   public Text pointsText;
    
-// }
+   // Start off with 0 points 
+   int Score = 0;
+
+    //Starting to earn points 
+    void Start(){
+        QuizManager point;
+        GetPoints(point);
+        
+        //scoreText.text = point.getCorrect() + "Points";
+    }
+
+    public Text pointText;
+    
+
+    public void GetPoints(GameObject point){
+        point = GameObject.Find("Score").GetComponent<QuizManager>();
+        pointsText.text = point.getCorrect().ToString();
+    }
+   
+}
 
 
    
