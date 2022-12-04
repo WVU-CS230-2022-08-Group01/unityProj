f73f7bf1f5e3cb63fd7b50856a50f8db9d39a803
BrianTech09 <90514604+BrianTech09@users.noreply.github.com>
2022-12-04 14:16:20 -0500

    Comment
    SM

--- Assets/Scenes/Scripts/ScoringManage.cs
+++ Assets/Scenes/Scripts/ScoringManage.cs
@@ -1,31 +1,31 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+// using UnityEngine.UI;
 
-public class ScoringManage : MonoBehaviour
-{
-    public Text pointsText; 
-    public static ScoringManage instance;
-    Score board = GameObject.Find("Score").GetComponent<Score>();
+// public class ScoringManage : MonoBehaviour
+// {
+//     public Text pointsText; 
+//     public static ScoringManage instance;
+//     Score board = GameObject.Find("Score").GetComponent<Score>();
    
-    void Start(){
-        int Score = 0;
-        //QuizManager point;
-        GetPoints(point);
-    }
+//     void Start(){
+//         int Score = 0;
+//         //QuizManager point;
+//         GetPoints(point);
+//     }
 
-    public void AddPoints(){
-        Score.totalScore += 1;
-        //scoreText.text = point.getCorrect() + "Points";
-    }
+//     public void AddPoints(){
+//         Score.totalScore += 1;
+//         //scoreText.text = point.getCorrect() + "Points";
+//     }
     
-    public void GetPoints(GameObject point){
-        point = GameObject.Find("Score").GetComponent<QuizManager>();
-        pointsText.text = point.getCorrect().ToString();
-    }
+//     public void GetPoints(GameObject point){
+//         point = GameObject.Find("Score").GetComponent<QuizManager>();
+//         pointsText.text = point.getCorrect().ToString();
+//     }
    
-}
+// }
 
 
    
